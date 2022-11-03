3c4a0e5409b5ad22ef18955a1f671aa1280e0ec1
Chris Friedt <cfriedt@meta.com>
2022-11-02 21:55:48 -0400

    boards: arm64: qemu_cortex_a53: add Jiri Slaby's EDU device
    Add support for the DMA on Jiri Slaby's EDU device.
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- boards/arm64/qemu_cortex_a53/board.cmake
+++ boards/arm64/qemu_cortex_a53/board.cmake
@@ -18,6 +18,25 @@ set(QEMU_FLAGS_${ARCH}
   -machine ${QEMU_MACH}
   )
 
+  if(CONFIG_PCIE)
+  # PCI test device
+  set(QEMU_FLAGS_${ARCH}
+  ${QEMU_FLAGS_${ARCH}}
+  -device pci-testdev,bus=pcie.0		# 00:01:0
+  -device edu,bus=pcie.0,dma_mask=0x1FFFFFFFFFF # 00:02:0
+  )
+  set(QEMU_FLAGS_${ARCH}
+  ${QEMU_FLAGS_${ARCH}}
+   # 2 bridges on same DEV id with edu on each of them
+  -device ioh3420,id=root_port1,chassis=1,slot=1,multifunction=on,addr=3.0	# 00:03:0
+        -device edu,bus=root_port1,dma_mask=0x1FFFFFFFFFF			# 01:00:0
+
+  # on 00:03:0	-> dev 1
+        -device ioh3420,id=root_port11,chassis=1,slot=3,bus=root_port1		# 01:01:0
+                -device edu,bus=root_port11,dma_mask=0x1FFFFFFFFFF		# 02:00:0
+  )
+endif()
+
 if(CONFIG_XIP)
   # This should be equivalent to
   #   ... -drive if=pflash,file=build/zephyr/zephyr.bin,format=raw
