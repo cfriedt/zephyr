f94031010640c6df06f57ebf9e586ba20bae0962
Chris Friedt <cfriedt@tenstorrent.com>
2024-06-28 23:43:53 -0400

    modify hello world to wait for a button to be pressed and then find a firmware update on external flash, then update ither slot0 or slot1

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,9 +6,29 @@
 
 #include <stdio.h>
 
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
+
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	/*
+	 * Register a callback for when the user button is pressed
+	 * This can really be any kind of asynchronous event handler.
+	 */
+	static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+	static struct gpio_callback button_cb_data;
+
+	gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
 
 	return 0;
 }
