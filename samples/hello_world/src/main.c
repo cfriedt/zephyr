c32570aeffddf083ba122db913c92e0b8215b9dc
Chris Friedt <cfriedt@tenstorrent.com>
2024-07-04 07:38:04 -0400

    stashing

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
@@ -22,13 +23,31 @@ int main(void)
 	 * Register a callback for when the user button is pressed
 	 * This can really be any kind of asynchronous event handler.
 	 */
-	static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+	int ret;
 	static struct gpio_callback button_cb_data;
+	static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
 
-	gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-	gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) {
+		errno = -ret;
+		perror("gpio_pin_configure_dt");
+		return EXIT_FAILURE;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		errno = -ret;
+		perror("gpio_pin_interrupt_configure_dt");
+		return EXIT_FAILURE;
+	}
+
 	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
+	ret = gpio_add_callback(button.port, &button_cb_data);
+	if (ret < 0) {
+		errno = -ret;
+		perror("gpio_add_callback");
+		return EXIT_FAILURE;
+	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
