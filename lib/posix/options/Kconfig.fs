f0edb81350b8070eb6259b0034fbe016fe91f57e
Chris Friedt <cfriedt@tenstorrent.com>
2024-11-16 11:59:58 -0500

    stashing

--- lib/posix/options/Kconfig.fs
+++ lib/posix/options/Kconfig.fs
@@ -2,20 +2,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-menuconfig POSIX_FILE_SYSTEM
+menu "POSIX file system API support"
+
+config POSIX_FILE_SYSTEM
 	bool "POSIX file system API support"
 	default y if POSIX_API
 	select FILE_SYSTEM
 	select FDTABLE
 	help
 	  This enables POSIX style file system related APIs.
-
-if POSIX_FILE_SYSTEM
-
-config POSIX_FILE_SYSTEM_ALIAS_FSTAT
-	bool
-	help
-	  When selected via Kconfig, Zephyr will provide an alias for fstat() as _fstat().
 
 config POSIX_FILE_SYSTEM_R
 	bool "Thread-Safe File System"
@@ -25,5 +20,12 @@ config POSIX_FILE_SYSTEM_R
 
 	  For more informnation, please see
 	  https://pubs.opengroup.org/onlinepubs/9699919799/xrat/V4_subprofiles.html
+
+if POSIX_FILE_SYSTEM
+
+config POSIX_FILE_SYSTEM_ALIAS_FSTAT
+	bool
+	help
+	  When selected via Kconfig, Zephyr will provide an alias for fstat() as _fstat().
 
 endif # POSIX_FILE_SYSTEM
