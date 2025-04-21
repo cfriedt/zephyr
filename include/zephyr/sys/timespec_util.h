44f27c2f185e583db97c770b64d99defcc626a47
Chris Friedt <cfriedt@tenstorrent.com>
2025-04-21 13:43:30 -0400

    blah

--- include/zephyr/sys/timespec_util.h
+++ include/zephyr/sys/timespec_util.h
@@ -6,6 +6,7 @@
 #ifndef ZEPHYR_INCLUDE_SYS_TSUTIL_H_
 #define ZEPHYR_INCLUDE_SYS_TSUTIL_H_
 
+#include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -18,6 +19,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+BUILD_ASSERT(sizeof(((struct timespec *)0)->tv_sec) == sizeof(int64_t), "tv_sec must be 64-bit");
+
+/**
+ * @defgroup timespec_util_apis Timespec Utility APIs
+ * @ingroup utilities
+ * @defgroup timespec_util_repr_apis Timespec Representation APIs
+ * @ingroup timespec_util_apis
+ * @{
+ */
 
 /**
  * @brief Check if a timespec is valid.
@@ -38,9 +49,7 @@ static inline bool timespec_is_valid(const struct timespec *ts)
 	return (ts->tv_nsec >= 0) && (ts->tv_nsec < NSEC_PER_SEC);
 }
 
-/* #if (__STDC_VERSION__ >= 201112L) && HAS_BUILTIN(__builtin_add_overflow) || defined(__DOXYGEN__)
- */
-#if 0
+#if (__STDC_VERSION__ >= 201112L) && HAS_BUILTIN(__builtin_add_overflow) || defined(__DOXYGEN__)
 /**
  * @brief Normalize a timespec so that the `tv_nsec` field is in valid range.
  *
@@ -50,7 +59,7 @@ static inline bool timespec_is_valid(const struct timespec *ts)
  * negative direction when `tv_nsec` < 0.
  *
  * In pseudocode, normalization can be done as follows:
- * ```
+ * ```python
  * if ts.tv_nsec >= NSEC_PER_SEC:
  *     sec = ts.tv_nsec / NSEC_PER_SEC
  *     ts.tv_sec += sec
@@ -61,15 +70,15 @@ static inline bool timespec_is_valid(const struct timespec *ts)
  *	    ts.tv_sec -= sec;
  *	    ts.tv_nsec += sec * NSEC_PER_SEC;
  * ```
- * 
- * @note there are two cases where the normalization can result in integer overflow. These can
+ *
+ * @note There are two cases where the normalization can result in integer overflow. These can
  * be extrapolated to not simply overflowing the `tv_sec` field by one second, but also by any
  * realizable multiple of `NSEC_PER_SEC`.
- * 
- * 1. When `tv_nsec` is negative and `tv_sec` is already most positive.
- * 2. When `tv_nsec` is greater-or-equal to `NSEC_PER_SEC` and `tv_sec` is already most negative.
- * 
- * @note @p ts must not be `NULL`.
+ *
+ * 1. When `tv_nsec` is negative and `tv_sec` is already most negative.
+ * 2. When `tv_nsec` is greater-or-equal to `NSEC_PER_SEC` and `tv_sec` is already most positive.
+ *
+ * @note @p ts must be non-`NULL`.
  *
  * @param ts the timespec to be normalized
  *
@@ -84,16 +93,12 @@ static inline bool timespec_normalize(struct timespec *ts)
 	 * prediction.
 	 */
 
-	long sign = (ts->tv_nsec >= 0) - (ts->tv_nsec < 0);
-	long sec = (ts->tv_nsec >= NSEC_PER_SEC) * (ts->tv_nsec / NSEC_PER_SEC) +
-		   (ts->tv_nsec < 0) * DIV_ROUND_UP(-ts->tv_nsec, NSEC_PER_SEC);
+	int sign = (ts->tv_nsec >= 0) - (ts->tv_nsec < 0);
+	int64_t sec = (ts->tv_nsec >= NSEC_PER_SEC) * (ts->tv_nsec / NSEC_PER_SEC) +
+		      (ts->tv_nsec < 0) * DIV_ROUND_UP(-ts->tv_nsec, NSEC_PER_SEC);
 
-	/*
-	 * Note: __builtin_add_overflow() uses C11 generics and works for signed types as well as
-	 * 4- or 8-byte integers.
-	 */
 	return __builtin_add_overflow(ts->tv_sec, sign * sec, &ts->tv_sec) ||
-	       __builtin_sub_overflow(ts->tv_nsec, sign * sec * NSEC_PER_SEC, &ts->tv_nsec);
+	       __builtin_sub_overflow(ts->tv_nsec, (long)(sign * sec * NSEC_PER_SEC), &ts->tv_nsec);
 }
 
 /**
@@ -102,7 +107,7 @@ static inline bool timespec_normalize(struct timespec *ts)
  * This function sums the two timespecs pointed to by @p a and @p b and stores the result in the
  * timespce pointed to by @p a.
  *
- * @note @p a and @p b must not be `NULL`.
+ * @note @p a and @p b must be non-`NULL`.
  *
  * @param a the timespec which is added to
  * @param b the timespec to be added
@@ -118,48 +123,54 @@ static inline bool timespec_add(struct timespec *a, const struct timespec *b)
 	       __builtin_add_overflow(a->tv_nsec, b->tv_nsec, &a->tv_nsec) || timespec_normalize(a);
 }
 
+/**
+ * @brief Negate a timespec object
+ *
+ * Negate the timespec object pointed to be @p ts and store the result in the same
+ * memory location.
+ *
+ * @param ts The timespec object to negate.
+ *
+ * @return `true` of the operation would overflow, otherwise `false`.
+ */
+static inline bool timespec_neg(struct timespec *ts)
+{
+	__ASSERT_NO_MSG(ts != NULL);
+
+	return __builtin_sub_overflow(0LL, ts->tv_sec, &ts->tv_sec) ||
+	       __builtin_sub_overflow(0L, ts->tv_nsec, &ts->tv_nsec) || timespec_normalize(ts);
+}
 #else
 
 static inline bool timespec_normalize(struct timespec *ts)
 {
-    __ASSERT_NO_MSG(ts != NULL);
+	__ASSERT_NO_MSG(ts != NULL);
 
-    /* This implementation is unoptimized mainly for the purpose of comprehension */
+	/* This implementation is unoptimized mainly for readability */
 
-    long sec;
+	long sec = 0;
 
-    if (ts->tv_nsec >= NSEC_PER_SEC) {
-	    sec = ts->tv_nsec / NSEC_PER_SEC;
-    } else if (ts->tv_nsec < 0) {
-	    sec = DIV_ROUND_UP(-ts->tv_nsec, NSEC_PER_SEC);
-    }
-
-	if (sizeof((ts->tv_sec) == sizeof(int32_t))) {
-		if ((ts->tv_nsec < 0) && (ts->tv_sec >= 0) &&
-		    ((INT32_MAX - (int32_t)ts->tv_sec) < sec)) {
-			/*
-			 * When `tv_nsec` is negative and `tv_sec` is already most positive.
-			 */
-
-			/* adding sec would result in positive integer overflow */
-			return true;
-		}
-		if ((ts->tv_nsec >= NSEC_PER_SEC) && (ts->tv_sec < 0) &&
-		    (((int32_t)ts->tv_sec) - INT32_MIN >= sec)) {
-			/* subtracting sec would result in negative integer overflow */
-			return true;
-		}
+	if (ts->tv_nsec >= NSEC_PER_SEC) {
+		sec = ts->tv_nsec / NSEC_PER_SEC;
+	} else if (ts->tv_nsec < 0) {
+		sec = DIV_ROUND_UP(-ts->tv_nsec, NSEC_PER_SEC);
 	}
 
 	if (sizeof((ts->tv_sec) == sizeof(int64_t))) {
-		if ((ts->tv_nsec < 0) && (ts->tv_sec >= 0) &&
-		    ((INT64_MAX - (int64_t)ts->tv_sec) >= sec)) {
-			/* adding sec would result in positive integer overflow */
+		if ((ts->tv_nsec < 0) && (ts->tv_sec < 0) && (ts->tv_sec - INT64_MIN < sec)) {
+			/*
+			 * When `tv_nsec` is negative and `tv_sec` is already most negative,
+			 * further subtraction would cause integer overflow.
+			 */
 			return true;
 		}
-		if ((ts->tv_nsec >= NSEC_PER_SEC) && (ts->tv_sec < 0) &&
-		    (((int64_t)ts->tv_sec) - INT64_MIN >= sec)) {
-			/* subtracting sec would result in negative integer overflow */
+
+		if ((ts->tv_nsec >= NSEC_PER_SEC) && (ts->tv_sec > 0) &&
+		    (INT64_MAX - ts->tv_sec < sec)) {
+			/*
+			 * When `tv_nsec` is >= `NSEC_PER_SEC` and `tv_sec` is already most
+			 * positive, further addition would cause integer overflow.
+			 */
 			return true;
 		}
 	}
@@ -180,18 +191,83 @@ static inline bool timespec_add(struct timespec *a, const struct timespec *b)
 	__ASSERT_NO_MSG(a != NULL);
 	__ASSERT_NO_MSG(b != NULL);
 
-    /* FIXME: add bounds-checking for tv_sec */
-    /* FIXME: add bounds-checking for tv_nsec */
+	if ((a->tv_sec < 0) && (b->tv_sec < 0) && (INT64_MIN - a->tv_sec < -b->tv_sec)) {
+		/* negative integer overflow would occur */
+		return true;
+	}
+
+	if ((a->tv_sec > 0) && (b->tv_sec > 0) && (INT64_MAX - a->tv_sec < b->tv_sec)) {
+		/* positive integer overflow would occur */
+		return true;
+	}
+
+	if ((a->tv_nsec < 0) && (b->tv_nsec < 0) && (LONG_MIN - a->tv_nsec < -b->tv_nsec)) {
+		/* negative integer overflow would occur */
+		return true;
+	}
+
+	if ((a->tv_nsec > 0) && (b->tv_nsec > 0) && (LONG_MAX - a->tv_nsec < b->tv_nsec)) {
+		/* positive integer overflow would occur */
+		return true;
+	}
 
 	a->tv_sec += b->tv_sec;
 	a->tv_nsec += b->tv_nsec;
 
 	return timespec_normalize(a);
 }
-#endif /* (__STDC_VERSION__ >= 201112L) && HAS_BUILTIN(__builtin_add_overflow) || defined(__DOXYGEN__) */
+
+static inline bool timespec_neg(struct timespec *ts)
+{
+	__ASSERT_NO_MSG(ts != NULL);
+
+	if (ts->tv_sec == INT64_MIN) {
+		/* -INT64_MIN > INT64_MAX, so +ve integer overflow would occur */
+		return true;
+	}
+
+	if (ts->tv_nsec == LONG_MIN) {
+		/* -LONG_MIN > LONG_MAX, so +ve integer overflow would occur */
+		return true;
+	}
+
+	ts->tv_sec = -ts->tv_sec;
+	ts->tv_nsec = -ts->tv_nsec;
+
+	return timespec_normalize(ts);
+}
+#endif /* (__STDC_VERSION__ >= 201112L) && HAS_BUILTIN(__builtin_add_overflow) ||                  \
+	  defined(__DOXYGEN__) */
+
+/**
+ * @brief Subtract one timespec from another
+ *
+ * This function subtracts the timespec pointed to by @p b from the timespec pointed to by @p b and
+ * stores the result in the timespce pointed to by @p a.
+ *
+ * @note @p a and @p b must be non-`NULL`.
+ *
+ * @param a the timespec which is subtracted from
+ * @param b the timespec to be subtracted
+ *
+ * @return `true` if the operation would result in integer overflow, otherwise `false`.
+ */
+static inline bool timespec_sub(struct timespec *a, const struct timespec *b)
+{
+	__ASSERT_NO_MSG(a != NULL);
+	__ASSERT_NO_MSG(b != NULL);
+
+	struct timespec neg = *b;
+
+	return timespec_neg(&neg) || timespec_add(a, &neg);
+}
 
 #ifdef __cplusplus
 }
 #endif
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_SYS_TSUTIL_H_ */
--- tests/lib/time/src/timespec.c
+++ tests/lib/time/src/timespec.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <inttypes.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <time.h>
 
@@ -12,6 +14,10 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/timeutil.h>
 #include <zephyr/sys/timespec_util.h>
+
+/* Normally NSEC_PER_SEC is defined as UL, which breaks static declarations below */
+#undef NSEC_PER_SEC
+#define NSEC_PER_SEC 1000000000LL
 
 /*
  * test spec for simple timespec validation
@@ -33,7 +39,17 @@ struct ts_test_spec {
  */
 #define DECL_VALID_TS_TEST(sec, nsec)                                                              \
 	{                                                                                          \
-		.invalid_ts = {(sec), (nsec)}, .valid_ts = {(sec), (nsec)}, .expect_valid = true,  \
+		.invalid_ts =                                                                      \
+			{                                                                          \
+				.tv_sec = (sec),                                                   \
+				.tv_nsec = (nsec),                                                 \
+			},                                                                         \
+		.valid_ts =                                                                        \
+			{                                                                          \
+				.tv_sec = (sec),                                                   \
+				.tv_nsec = (nsec),                                                 \
+			},                                                                         \
+		.expect_valid = true,                                                              \
 	}
 
 /*
@@ -46,8 +62,17 @@ struct ts_test_spec {
  */
 #define DECL_INVALID_TS_TEST(invalid_sec, invalid_nsec, valid_sec, valid_nsec)                     \
 	{                                                                                          \
-		.valid_ts = {(valid_sec), (valid_nsec)},                                           \
-		.invalid_ts = {(invalid_sec), (invalid_nsec)}, .expect_valid = false,              \
+		.valid_ts =                                                                        \
+			{                                                                          \
+				.tv_sec = (valid_sec),                                             \
+				.tv_nsec = (valid_nsec),                                           \
+			},                                                                         \
+		.invalid_ts =                                                                      \
+			{                                                                          \
+				.tv_sec = (invalid_sec),                                           \
+				.tv_nsec = (invalid_nsec),                                         \
+			},                                                                         \
+		.expect_valid = false,                                                             \
 	}
 
 /*
@@ -55,15 +80,16 @@ struct ts_test_spec {
  */
 #define DECL_OVFLW_TS_TEST(invalid_sec, invalid_nsec)                                              \
 	{                                                                                          \
-		.invalid_ts = {(invalid_sec), (invalid_nsec)}, .expect_valid = false,              \
-		.expect_ovflw = true,                                                              \
+		.invalid_ts =                                                                      \
+			{                                                                          \
+				.tv_sec = (invalid_sec),                                           \
+				.tv_nsec = (invalid_nsec),                                         \
+			},                                                                         \
+		.expect_valid = false, .expect_ovflw = true,                                       \
 	}
 
 /*
  * Easily verifiable tests for both valid and invalid timespecs.
- *
- * Note: ISO C does not specify minimum / maximum values for time_t so we only include tests
- * with preprocessor constant values in the int32_t range below.
  *
  * There are tests for the full range of tv_sec and tv_nsec further down in this file, but
  * they use compile-time checks.
@@ -80,33 +106,42 @@ static const struct ts_test_spec ts_tests[] = {
 	DECL_VALID_TS_TEST(-1, 0),
 	DECL_VALID_TS_TEST(-1, 1),
 	DECL_VALID_TS_TEST(-1, NSEC_PER_SEC - 1),
-	DECL_VALID_TS_TEST(INT32_MIN, 0),
-	DECL_VALID_TS_TEST(INT32_MIN, NSEC_PER_SEC - 1),
-	DECL_VALID_TS_TEST(INT32_MAX, 0),
-	DECL_VALID_TS_TEST(INT32_MAX, NSEC_PER_SEC - 1),
+	DECL_VALID_TS_TEST(INT64_MIN, 0),
+	DECL_VALID_TS_TEST(INT64_MIN, NSEC_PER_SEC - 1),
+	DECL_VALID_TS_TEST(INT64_MAX, 0),
+	DECL_VALID_TS_TEST(INT64_MAX, NSEC_PER_SEC - 1),
 
 	/* Correctable, invalid cases */
 	DECL_INVALID_TS_TEST(0, -1, -1, NSEC_PER_SEC - 1),
 	DECL_INVALID_TS_TEST(0, NSEC_PER_SEC, 1, 0),
 	DECL_INVALID_TS_TEST(1, -1, 0, NSEC_PER_SEC - 1),
 	DECL_INVALID_TS_TEST(1, NSEC_PER_SEC, 2, 0),
+
 	DECL_INVALID_TS_TEST(-1, -1, -2, NSEC_PER_SEC - 1),
 	DECL_INVALID_TS_TEST(0, NSEC_PER_SEC, 1, 0),
 	DECL_INVALID_TS_TEST(1, -1, 0, NSEC_PER_SEC - 1),
 	DECL_INVALID_TS_TEST(1, NSEC_PER_SEC, 2, 0),
 
-	DECL_INVALID_TS_TEST(INT32_MIN, NSEC_PER_SEC, INT32_MIN + 1, 0),
-	DECL_INVALID_TS_TEST(INT32_MAX, -1, INT32_MAX - 1, NSEC_PER_SEC - 1),
+	DECL_INVALID_TS_TEST(INT64_MIN, NSEC_PER_SEC, INT64_MIN + 1, 0),
+	DECL_INVALID_TS_TEST(INT64_MAX, -1, INT64_MAX - 1, NSEC_PER_SEC - 1),
 
 	/* Incorrectable, invalid cases */
-	DECL_OVFLW_TS_TEST(INT32_MIN, -1),
-	DECL_OVFLW_TS_TEST(INT32_MIN + 1, -NSEC_PER_SEC),
-	DECL_OVFLW_TS_TEST(INT32_MIN + 2, -2 * NSEC_PER_SEC),
-	DECL_OVFLW_TS_TEST(INT32_MAX, NSEC_PER_SEC),
-	DECL_OVFLW_TS_TEST(INT32_MAX, NSEC_PER_SEC + 1),
-	DECL_OVFLW_TS_TEST(INT32_MAX - 1, 2 * NSEC_PER_SEC),
-	DECL_OVFLW_TS_TEST(INT32_MAX - 1, 2 * NSEC_PER_SEC + 1),
+	DECL_OVFLW_TS_TEST(INT64_MIN, -1),
+	DECL_OVFLW_TS_TEST(INT64_MIN, -NSEC_PER_SEC),
+	DECL_OVFLW_TS_TEST(INT64_MIN, -NSEC_PER_SEC - 1),
+	DECL_OVFLW_TS_TEST(INT64_MIN, -2 * NSEC_PER_SEC),
+	DECL_OVFLW_TS_TEST(INT64_MIN, -2 * NSEC_PER_SEC - 1),
+	DECL_OVFLW_TS_TEST(INT64_MIN + 1, -1 * NSEC_PER_SEC - 1),
+	DECL_OVFLW_TS_TEST(INT64_MIN + 2, -2 * NSEC_PER_SEC - 1),
+
+	DECL_OVFLW_TS_TEST(INT64_MAX, NSEC_PER_SEC),
+	DECL_OVFLW_TS_TEST(INT64_MAX, NSEC_PER_SEC + 1),
+	DECL_OVFLW_TS_TEST(INT64_MAX, 2 * NSEC_PER_SEC),
+	DECL_OVFLW_TS_TEST(INT64_MAX, 2 * NSEC_PER_SEC + 1),
+	DECL_OVFLW_TS_TEST(INT64_MAX - 1, 3 * NSEC_PER_SEC - 1),
+	DECL_OVFLW_TS_TEST(INT64_MAX - 2, 4 * NSEC_PER_SEC - 1),
 };
+BUILD_ASSERT(sizeof(((struct timespec *)0)->tv_sec) == sizeof(int64_t), "tv_sec must be 64 bits");
 
 ZTEST(timespec, test_timespac_is_valid)
 {
@@ -117,17 +152,19 @@ ZTEST(timespec, test_timespac_is_valid)
 
 		if (tspec->expect_valid) {
 			zexpect_equal(valid, true,
-				      "timespec_is_valid({%ld, %ld}) = false, expected true",
+				      "timespec_is_valid({%" PRId64
+				      ", %ld}) = false, expected true",
 				      ts->tv_sec, ts->tv_nsec);
 		} else {
 			zexpect_equal(valid, false,
-				      "timespec_is_valid({%ld, %ld}) = true, expected false",
+				      "timespec_is_valid({%" PRId64
+				      ", %ld}) = true, expected false",
 				      ts->tv_sec, ts->tv_nsec);
 		}
 	}
 }
 
-ZTEST(timespec, test_timespac_normalize)
+ZTEST(timespec, test_timespec_normalize)
 {
 	ARRAY_FOR_EACH(ts_tests, i) {
 		struct timespec norm;
@@ -135,21 +172,22 @@ ZTEST(timespec, test_timespac_normalize)
 		const struct timespec *const valid = &tspec->valid_ts;
 		const struct timespec *const invalid = &tspec->invalid_ts;
 
-		zassert_true(timespec_is_valid(valid), "Valid timespec {%ld, %ld} is not valid",
-			     valid->tv_sec, valid->tv_nsec);
+		zexpect_true(timespec_is_valid(valid),
+			     "Valid timespec {%" PRId64 ", %ld} is not valid", valid->tv_sec,
+			     valid->tv_nsec);
 
 		if (tspec->expect_ovflw) {
-			/* if we expect normalization should overflow, then they should be invalid
-			 */
-			zassert_false(timespec_is_valid(invalid),
-				      "Invalid timespec {%ld, %ld} is not invalid", invalid->tv_sec,
-				      invalid->tv_nsec);
+			/* by definition, invalid, and normalize should overflow */
+			zexpect_false(timespec_is_valid(invalid),
+				      "Invalid timespec {%" PRId64 ", %ld} is not invalid",
+				      (int64_t)invalid->tv_sec, (int64_t)invalid->tv_nsec);
 
-			/* normalize should overflow */
-			norm = *invalid;
-			zassert_true(timespec_normalize(&norm),
-				     "timespec_normalize({%ld, %ld}) did not overflow",
-				     tspec->invalid_ts.tv_sec, tspec->invalid_ts.tv_nsec);
+			norm.tv_sec = invalid->tv_sec;
+			norm.tv_nsec = invalid->tv_nsec;
+			zexpect_true(timespec_normalize(&norm),
+				     "timespec_normalize({%" PRId64 ", %ld}) did not overflow",
+				     (int64_t)tspec->invalid_ts.tv_sec,
+				     (int64_t)tspec->invalid_ts.tv_nsec);
 
 			continue;
 		}
@@ -157,30 +195,132 @@ ZTEST(timespec, test_timespac_normalize)
 		/* either valid or correctable timespecs */
 		if (tspec->expect_valid) {
 			norm = *valid;
-			zassert_false(timespec_normalize(&norm),
-				      "timespec_normalize({%ld, %ld}) caused an overflow",
+			zexpect_false(timespec_normalize(&norm),
+				      "timespec_normalize({%" PRId64 ", %ld}) caused an overflow",
 				      valid->tv_sec, valid->tv_nsec);
 			zexpect_true((norm.tv_sec == valid->tv_sec) &&
 					     (norm.tv_nsec == valid->tv_nsec),
-				     "timespec_normalize({%ld, %ld}) modified a valid timespec to "
-				     "{%ld, %ld}",
+				     "timespec_normalize({%" PRId64
+				     ", %ld}) modified a valid timespec to "
+				     "{%" PRId64 ", %ld}",
 				     valid->tv_sec, valid->tv_nsec, norm.tv_sec, norm.tv_nsec);
 		} else {
-			zassert_false(timespec_is_valid(invalid),
-				      "Invalid timespec {%ld, %ld} is not valid", invalid->tv_sec,
-				      invalid->tv_nsec);
+			zexpect_false(timespec_is_valid(invalid),
+				      "Invalid timespec {%" PRId64 ", %ld} is not valid",
+				      invalid->tv_sec, invalid->tv_nsec);
 
 			norm = *invalid;
-			zassert_false(timespec_normalize(&norm),
-				      "timespec_normalize({%ld, %ld}) caused an overflow",
+			zexpect_false(timespec_normalize(&norm),
+				      "timespec_normalize({%" PRId64 ", %ld}) caused an overflow",
 				      invalid->tv_sec, invalid->tv_nsec);
 			zexpect_true((norm.tv_sec == valid->tv_sec) &&
 					     (norm.tv_nsec == valid->tv_nsec),
-				     "timespec_normalize({%ld, %ld}) did not result in the "
-				     "expected valid timespec {%ld, %ld}",
+				     "timespec_normalize({%" PRId64 ", %ld}) did not result in the "
+				     "expected valid timespec {%" PRId64 ", %ld}",
 				     norm.tv_sec, norm.tv_nsec, valid->tv_sec, valid->tv_nsec);
 		}
 	}
 }
+
+ZTEST(timespec, test_timespec_neg)
+{
+	struct test_ts_neg_spec {
+		struct timespec ts;
+		struct timespec neg;
+		bool expect_ovflw;
+	};
+
+	struct timespec res;
+	static const struct test_ts_neg_spec specs[] = {
+		{{INT64_MIN, LONG_MIN}, {0, 0}, true},
+		{{INT64_MIN, -NSEC_PER_SEC - 1}, {0, 0}, true},
+		{{INT64_MIN, -NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MIN, -NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MIN, 0}, {0, 0}, true},
+		{{INT64_MIN, NSEC_PER_SEC - 1}, {0, 0}, true},
+		{{INT64_MIN, NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MIN, NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MAX}, {0, 0}, true},
+
+		{{0, LONG_MIN}, {0, 0}, false},
+		{{0, -NSEC_PER_SEC - 1}, {0, 0}, false},
+		{{0, -NSEC_PER_SEC}, {0, 0}, false},
+		{{0, -NSEC_PER_SEC + 1}, {0, 0}, false},
+		{{0, 0}, {0, 0}, false},
+		{{0, NSEC_PER_SEC - 1}, {0, 0}, false},
+		{{0, NSEC_PER_SEC}, {0, 0}, false},
+		{{0, NSEC_PER_SEC + 1}, {0, 0}, false},
+		{{0, LONG_MAX}, {0, 0}, false},
+
+		{{INT64_MAX, LONG_MIN}, {0, 0}, true},
+		{{INT64_MAX, -NSEC_PER_SEC - 1}, {0, 0}, true},
+		{{INT64_MAX, -NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MAX, -NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MAX, 0}, {0, 0}, false},
+		{{INT64_MAX, NSEC_PER_SEC - 1}, {0, 0}, true},
+		{{INT64_MAX, NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MAX, NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MAX, LONG_MAX}, {0, 0}, true},
+	};
+
+	ARRAY_FOR_EACH(ts_tests, i) {
+		const struct test_ts_neg_spec *const tspec = &specs[i];
+		const struct timespec *const ts = &tspec->ts;
+		const struct timespec *const neg = &tspec->neg;
+
+		res = *ts;
+		if (tspec->expect_ovflw) {
+			res = *ts;
+			zexpect_true(timespec_neg(&res),
+				     "timespec_neg({%" PRId64 ", %ld}) did not overflow",
+				     ts->tv_sec, ts->tv_nsec);
+			continue;
+		}
+
+		zexpect_false(timespec_neg(&res),
+			      "timespec_neg({%" PRId64 ", %ld}) caused an overflow", ts->tv_sec,
+			      ts->tv_nsec);
+		zexpect_equal(res.tv_sec, neg->tv_sec,
+			      "timespec_neg({%" PRId64 ", %ld}) did not result in the expected "
+			      "negated timespec {%" PRId64 ", %ld}",
+			      ts->tv_sec, ts->tv_nsec, neg->tv_sec, neg->tv_nsec);
+	}
+}
+
+#if 0
+ZTEST(timespec, test_timespec_add)
+{
+	struct test_ts_add_spec {
+		struct timespec a;
+		struct timespec b;
+		struct timespec c;
+		bool expect_ovflw;
+	};
+
+	struct timespec res;
+	static const struct test_ts_add_spec ts_add_specs[] = {
+		/* There are many ways to generate both negative and positive overflows using both
+		   inputs */
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, LONG_MIN}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, -NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, -NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, -1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, 0}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, LONG_MAX}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {-1, LONG_MIN}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, -NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, -NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, -1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, 0}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, NSEC_PER_SEC}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, NSEC_PER_SEC + 1}, {0, 0}, true},
+		{{INT64_MIN, LONG_MIN}, {INT64_MIN, LONG_MAX}, {0, 0}, true},
+	};
+}
+#endif
 
 ZTEST_SUITE(timespec, NULL, NULL, NULL, NULL, NULL);
