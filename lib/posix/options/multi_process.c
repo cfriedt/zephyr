ba14b6c0a68415a94566fee589c762c8fca77543
Chris Friedt <cfriedt@tenstorrent.com>
2025-10-10 08:44:40 -0400

    posix: multi process: add support for times()
    Add support for the `times()` function, which can be used to get the
    number of ticks spent in "system" and "user" mode, and which returns
    the "real time" that has expired, since an arbitrary point in the past.
    
    The following invariant should always hold:
    
    rtime <= stime + utime
    
    The `times()` function measures time for the calling process and all
    of it's child processes. In Zephyr, we don't support separate processes
    (yet), so the time spent in child processes is zero.
    
    Additionally, in Zephyr we do not differentiate between "system" time
    (i.e. time spent executing kernel code) and "user" time (time spent
    executing application code). We only have information on "total time"
    (time spent executing code) and "execution time" (time spent executing
    code plus idle time). So unfortunately, system time (in this case), is
    equivalent to idle time.
    
    At some point, these poor approximations should be fixed, but in any
    case, the invariant should remain true.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- lib/posix/options/multi_process.c
+++ lib/posix/options/multi_process.c
@@ -4,7 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <errno.h>
+#include <time.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/posix/sys/times.h>
 #include <zephyr/posix/unistd.h>
+#include <zephyr/sys/clock.h>
+#include <zephyr/sys/time_units.h>
+#include <zephyr/sys/util.h>
 
 pid_t getpid(void)
 {
@@ -24,3 +32,39 @@ pid_t getpid(void)
 #ifdef CONFIG_POSIX_MULTI_PROCESS_ALIAS_GETPID
 FUNC_ALIAS(getpid, _getpid, pid_t);
 #endif /* CONFIG_POSIX_MULTI_PROCESS_ALIAS_GETPID */
+
+clock_t times(struct tms *buffer)
+{
+	int ret;
+	clock_t rtime; /* elapsed real time in clock ticks */
+	clock_t stime; /* system time */
+	clock_t utime; /* user time */
+	k_thread_runtime_stats_t stats;
+
+	ret = k_thread_runtime_stats_all_get(&stats);
+	if (ret < 0) {
+		errno = -ret;
+		return (clock_t)-1;
+	}
+
+	/* "system time" + non-idle + idle seconds */
+	stime = z_tmcvt(stats.execution_cycles, sys_clock_hw_cycles_per_sec(), USEC_PER_SEC,
+			IS_ENABLED(CONFIG_TIMER_READS_ITS_FREQUENCY_AT_RUNTIME) ? false : true,
+			sizeof(clock_t) == sizeof(uint32_t), false, false);
+
+	/* "user time" = non-idle cycles */
+	utime = z_tmcvt(stats.execution_cycles, sys_clock_hw_cycles_per_sec(), USEC_PER_SEC,
+			IS_ENABLED(CONFIG_TIMER_READS_ITS_FREQUENCY_AT_RUNTIME) ? false : true,
+			sizeof(clock_t) == sizeof(uint32_t), false, false);
+
+	rtime = stime;
+
+	*buffer = (struct tms){
+		.tms_utime = utime,
+		.tms_stime = stime,
+		.tms_cutime = 0,
+		.tms_cstime = 0,
+	};
+
+	return rtime;
+}
