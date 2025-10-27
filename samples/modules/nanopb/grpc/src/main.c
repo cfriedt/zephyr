a9d0b74e9cfe48496c3392a7d355c6af9dbf1376
Chris Friedt <cfriedt@tenstorrent.com>
2025-10-27 07:42:48 -0400

    stashing

---
+++ samples/modules/nanopb/grpc/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2011 Petteri Aimonen
+ * Copyright (c) 2021 Basalte bv
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include "src/hello.pb.h"
+
+int main()
+{
+	return 0;
+}
